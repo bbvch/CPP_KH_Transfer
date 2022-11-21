0d0b82757608d8d35f478a544f2c4a51a1d059ec
Christian Fischer <cfischer@schaerer.com>
2022-11-21 16:36:39 +0100

    Add example std tuple, std get

--- src/std_tuple_std_get.cpp
+++ src/std_tuple_std_get.cpp
@@ -1,5 +1,29 @@
 // SPDX-License-Identifier: GPL-3.0
 
+#include <iostream>
+#include <tuple>
+
+std::tuple<bool, int, std::string> getNumberOfGrindersViaRpc(bool isConnected) {
+    if(isConnected)
+        return {true, 3, "Successful"};
+    return {false, 0, "Not connected"};
+}
+
 int main(int, char **) {
+    std::cout << std::boolalpha;
+    auto resultOk = getNumberOfGrindersViaRpc(true);
+    {
+        std::cout << "\nFunction successfull: " << std::get<0>(resultOk);
+        std::cout << "\nNumber of grinders: " << std::get<1>(resultOk);
+        // this does not compile since there are only 0..2 available
+        // std::cout << "\nError message: " << std::get<3>(resultOk);
+    }
+    // since C++17
+    auto [successfull, numberOfGrinders, errorMessage] = getNumberOfGrindersViaRpc(false);
+    {
+        std::cout << "\nFunction successfull: " << successfull;
+        std::cout << "\nError message: " << errorMessage;
+    }
+    std::cout << std::endl;
     return 0;
 }
